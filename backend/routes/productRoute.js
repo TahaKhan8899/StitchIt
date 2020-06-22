33f4f9b314c439e380bbb483eb387c4dbda79c94
TahaKhan8899 <taha.khan8899@gmail.com>
2020-06-22 10:50:20 -0400

    Created API for products

---
+++ backend/routes/productRoute.js
@@ -0,0 +1,31 @@
+import express from 'express'
+import Product from '../models/productModel'
+import { getToken } from '../util'
+
+const router = express.Router()
+
+router.get('/', async (req, res) => {
+  const products = await Product.find({})
+  res.send(products)
+})
+
+router.post('/', async (req, res) => {
+  const product = new Product({
+    name: req.body.name,
+    price: req.body.name,
+    image: req.body.image,
+    brand: req.body.brand,
+    category: req.body.category,
+    inventoryCount: req.body.inventoryCount,
+    description: req.body.description,
+    rating: req.body.rating,
+    numReviews: req.body.numReviews,
+  })
+  const newProduct = await product.save()
+  if (newProduct) {
+    return res.statys(201).send({ message: 'New Product Created', data: newProduct })
+  }
+  return res.status(500).send({ message: 'Error in Creating a Product' })
+})
+
+export default router
--- backend/util.js
+++ backend/util.js
@@ -17,6 +17,31 @@ const getToken = (user) => {
   )
 }
 
+const isAuth = (req, res, next) => {
+  const token = req.headers.authorization
+  if (token) {
+    const onlyToken = token.slice(7, token.length)
+    jwt.verify(onlyToken, config.JWT_SECRET, (err, decode) => {
+      if (err) {
+        return res.status(401).send({ msg: 'Invalid Token' })
+      }
+      req.user = token
+      next()
+      return
+    })
+  }
+  return res.status(401).send({
+    msg: 'Token is not supplied',
+  })
+}
+
+const isAdmin = (req, res, next) => {
+  if (req.user && req.user.isAdmin) {
+    return next()
+  }
+  return res.status(401).send({ msg: 'Admin Token is not valid' })
+}
+
 const mongodbUrl = config.MONGODB_URL
 
 const checkDBConnection = () => {
@@ -41,4 +66,4 @@ const connectToMongoDB = () => {
   checkDBConnection()
 }
 
-export { getToken, connectToMongoDB }
+export { getToken, connectToMongoDB, isAuth, isAdmin }
