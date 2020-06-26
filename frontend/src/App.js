e92b72c38b78ad6f8189158be0e875aba63bbb5d
TahaKhan8899 <taha.khan8899@gmail.com>
2020-06-26 10:25:16 -0400

    Save progress

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -30,7 +30,7 @@ function App() {
             <Route path="/products/:id" component={Product} />
             <Route path="/createProduct" component={CreateProduct} />
             <Route path="/cart/:id?" component={Cart} />
-            <Route path="/checkout/:step" component={Checkout} />
+            <Route path="/checkout" component={Checkout} />
           </MainContent>
         </main>
         <Footer />
